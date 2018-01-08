c23cf4cd6b5c9e00cde94d9cc95f0089f863dfb2
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-01-08 17:05:18 +0100

    writing short random test

---
+++ tests/test_random.py
@@ -0,0 +1,12 @@
+import stempeg
+import numpy as np
+
+
+def test_shape():
+    R = np.random.random((5, 1000, 2))
+    stempeg.write_stems(R, "./random.stem.mp4")
+    S, rate = stempeg.read_stems(
+        "./random.stem.mp4"
+    )
+
+    assert S.shape == R.shape
