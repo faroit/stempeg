82918cd9b1f611fc6feec9769a9cb42be541fb08
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-01-08 19:19:04 +0100

    fixing tests for ffmpeg 2.8

--- tests/test_read.py
+++ tests/test_read.py
@@ -5,4 +5,6 @@ def test_shape():
     S, rate = stempeg.read_stems(
         "tests/data/The Easton Ellises - Falcon 69.stem.mp4"
     )
-    assert S.shape == (5, 265216, 2)
+    assert S.shape[0] == 5
+    assert ((S.shape[1] % 1024) == 0 and S.shape[1] > 200000)
+    assert S.shape[2] == 2
--- tests/test_write.py
+++ tests/test_write.py
@@ -5,6 +5,4 @@ def test_shape():
     S, rate = stempeg.read_stems(
         "tests/data/The Easton Ellises - Falcon 69.stem.mp4"
     )
-    assert S.shape == (5, 265216, 2)
-
     stempeg.write_stems(S, "./stems.mp4")
