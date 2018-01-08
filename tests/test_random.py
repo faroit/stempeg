1e216a465e4fda3703999f4e9d59abd0762beed6
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-01-08 19:41:32 +0100

    fix test for ffmpeg 2.8

--- tests/test_random.py
+++ tests/test_random.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def test_shape():
-    R = np.random.random((5, 2048, 2))
+    R = np.random.random((5, 3072, 2))
     stempeg.write_stems(R, "./random.stem.mp4")
     S, rate = stempeg.read_stems(
         "./random.stem.mp4"
