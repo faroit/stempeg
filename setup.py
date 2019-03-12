d92f7f1190f1c9dc2bbeca596bd78a4842ebbff1
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2019-03-12 18:01:16 +0100

    add support for 3.7

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Multimedia :: Sound/Audio :: Analysis',
             'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis'
         ],
