5490fa149f58b7e6475f492ab416321383b389bb
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2019-07-08 11:05:46 +0200

    drop support for python 3.3

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
             'Intended Audience :: Telecommunications Industry',
             'Intended Audience :: Science/Research',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
